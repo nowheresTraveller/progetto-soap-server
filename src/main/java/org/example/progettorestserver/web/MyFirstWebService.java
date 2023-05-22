c550faa354ba535176be8a23b3e43c587d6bfa47
GiovaCode89 <g.giovanni100@gmail.com>
2023-05-22 23:03:11 +0200

    seconda_commit

--- src/main/java/org/example/progettorestserver/web/MyFirstWebService.java
+++ src/main/java/org/example/progettorestserver/web/MyFirstWebService.java
@@ -5,6 +5,9 @@ import javax.jws.WebParam;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 
+
+
+
 @WebService
 @SOAPBinding(style = SOAPBinding.Style.RPC)
 public class MyFirstWebService {
